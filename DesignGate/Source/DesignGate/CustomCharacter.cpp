df2da4815fea0d6d12596cdfa7ba311a7ea79149
Cedric CHAUVIN <cedric.chauvin@edu.devinci.fr>
2021-03-08 12:10:10 +0100

    Feat(GP) : reimplement climb

---
+++ DesignGate/Source/DesignGate/CustomCharacter.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+ACustomCharacter::ACustomCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACustomCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	OnActorBeginOverlap.AddDynamic(this, &ACustomCharacter::ActorBeginOverlap);
+	OnActorEndOverlap.AddDynamic(this, &ACustomCharacter::ActorEndOverlap);
+}
+
+// Called every frame
+void ACustomCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if(isClimbing && wallActor != nullptr) {
+
+		float rotation = wallActor->GetActorRotation().GetComponentForAxis(EAxis::Z);
+		FRotator rotator = GetActorRotation();
+		rotator.SetComponentForAxis(EAxis::Z, rotation);
+		SetActorRotation(rotator);
+	}
+}
+
+// Called to bind functionality to input
+void ACustomCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ACustomCharacter::ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor->ActorHasTag("Climb")) {
+		GetCharacterMovement()->MovementMode = EMovementMode::MOVE_Flying;
+		GetCharacterMovement()->SetJumpAllowed(false);
+		isClimbing = true;
+		wallActor = OtherActor;
+		//GetCharacterMovement()->MaxFlySpeed = ClimbMaxSpeed;
+		JumpCurrentCount = 0;
+	}
+}
+
+void ACustomCharacter::ActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (OtherActor->ActorHasTag("Climb")) {
+		GetCharacterMovement()->MovementMode = EMovementMode::MOVE_Walking;
+		GetCharacterMovement()->SetJumpAllowed(true);
+		isClimbing = false;
+		wallActor = nullptr;
+		//GetCharacterMovement()->MaxFlySpeed = WalkMaxSpeed;
+	}
+}
---
+++ DesignGate/Source/DesignGate/CustomCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CustomCharacter.generated.h"
+
+UCLASS()
+class DESIGNGATE_API ACustomCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	UFUNCTION()
+	void ActorBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	UFUNCTION()
+	void ActorEndOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	
+public:
+	// Sets default values for this character's properties
+	ACustomCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Climb)
+	bool isClimbing = false;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Climb)
+	AActor* wallActor = nullptr;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

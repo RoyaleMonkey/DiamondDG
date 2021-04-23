3ab150fb04457b659fe1f937991bb9213ec47126
Thibautguerin <thibaut.guerin@edu.devinci.fr>
2021-04-23 14:42:56 +0200

    Fix(GP): Remove GamepadUMGPlugin of git
    TODO: All the team member need to add this plugin int Plugins folder!

--- DesignGate/Plugins/GamepadUMGPlugin_426/GamepadUMGPlugin/Intermediate/Build/Win64/UE4Editor/Development/GamepadUMGPlugin/PCH.GamepadUMGPlugin.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29913
-#include "D:/Projects/IIM/BigProjects/DiamondDG/DesignGate/Plugins/GamepadUMGPlugin_426/GamepadUMGPlugin/Intermediate/Build/Win64/UE4Editor/Development/GamepadUMGPlugin/PCH.GamepadUMGPlugin.h"

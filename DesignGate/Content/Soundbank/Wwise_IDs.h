365ed036236e119c611bdc0bd81d61a86be9012a
Jonathan <Urso.jonathan@gmail.com>
2021-04-27 21:06:57 +0200

    Add(SD) : Add asset audio(FT,Foley), BP foley notify, Material

--- DesignGate/Content/Soundbank/Wwise_IDs.h
+++ DesignGate/Content/Soundbank/Wwise_IDs.h
@@ -13,9 +13,20 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID FLUTE_C_END = 2246680465U;
+        static const AkUniqueID FLUTE_C_START = 984296002U;
+        static const AkUniqueID FLUTE_D_END = 2387598574U;
+        static const AkUniqueID FLUTE_D_START = 3839819269U;
+        static const AkUniqueID FLUTE_DD_END = 3115172146U;
+        static const AkUniqueID FLUTE_DD_START = 2920546321U;
+        static const AkUniqueID FLUTE_F_END = 3901381120U;
+        static const AkUniqueID FLUTE_F_START = 4214291931U;
+        static const AkUniqueID FLUTE_G_END = 1634689741U;
+        static const AkUniqueID FLUTE_G_START = 1149564230U;
         static const AkUniqueID PLAY_AMB = 2959533290U;
         static const AkUniqueID PLAY_AMB_FOUNTAIN = 570117905U;
         static const AkUniqueID PLAY_AMB_LAKE_LP = 832042763U;
+        static const AkUniqueID PLAY_MC_FOL_STEP = 4164330385U;
         static const AkUniqueID PLAY_MC_STEP = 3531921507U;
         static const AkUniqueID PLAY_MUSIC = 2932040671U;
         static const AkUniqueID STOP_AMB_LAKE = 3516178482U;
@@ -29,9 +40,13 @@ namespace AK
 
             namespace SWITCH
             {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS_LONG = 241501102U;
                 static const AkUniqueID GRASS_SHORT = 2705388594U;
                 static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WATER_PUDDLE = 3946787029U;
                 static const AkUniqueID WATER_SHALLOW = 122593115U;
+                static const AkUniqueID WOOD = 2058049674U;
             } // namespace SWITCH
         } // namespace MATERIAL
 
@@ -65,7 +80,10 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID FOLEYS = 4035004657U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MC = 1685527061U;
     } // namespace BUSSES
 
     namespace AUX_BUSSES
